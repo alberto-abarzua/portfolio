de0b4762d2a2f590c480045185a19d495f518019
alberto-abarzua <uintuser@pm.me>
2024-01-24 15:28:48 -0300

    chore: add linkedin to contact page

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,13 +1,23 @@
+import LinkedinSVG from '@/assets/svgs/linkedin.svg';
+import EmailSVG from '@/assets/svgs/email.svg';
+import GithubSVG from '@/assets/svgs/github.svg';
 const Contact = () => {
     return (
         <div className=" flex h-screen w-full flex-col items-center justify-center animate-in duration-300 fade-in slide-in-from-right">
             <h1 className="mb-5 font-mono text-5xl font-bold">Contact</h1>
-            <p className="font-mono text-lg">
-                <span className="font-bold "> Email: </span>contact [at] b3to [dot] dev
+            <p className="font-mono text-lg flex flex-row justifiy-center items-center">
+                <img src={EmailSVG} alt='Email' className="h-7 w-7 mr-2" />
+                <span className="font-bold mr-1"> Email: </span>contact [at] b3to [dot] dev
             </p>
-            <p className="font-mono text-lg">
-                <span className="font-bold "> Github: </span>{' '}
+            <p className="font-mono text-lg flex flex-row justifiy-center items-center">
+                <img src={GithubSVG} alt='Github' className="h-6 w-6 mr-2" />
+                <span className="font-bold mr-1"> Github: </span>{' '}
                 <a className='text-blue-900'  href="https://github.com/alberto-abarzua">alberto-abarzua</a>
+            </p>
+            <p className="font-mono text-lg flex flex-row justifiy-center items-center">
+                <img src={LinkedinSVG} alt='LinkedIn' className="h-7 w-7 mr-2" />
+                <span className="font-bold mr-1"> LinkedIn: </span>
+                <a className='text-blue-900'  href="https://www.linkedin.com/in/alberto-abarzua/"> My Profile</a>
             </p>
         </div>
     );
